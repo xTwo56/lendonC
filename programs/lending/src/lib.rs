3a5c13b1389cb5c4600d9727bdd579a78d6d8ba8
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2025-01-13 08:11:21 +0530

    feat: ix deposit added to lib.rs

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -8,6 +8,8 @@ declare_id!("PuzF3FCpbFa3GKdaTAhw7kS5uojzrMzk2EwfsZsZ2pi");
 
 #[program]
 pub mod lending {
+    use anchor_lang::solana_program::sysvar::instructions;
+
     use super::*;
 
     pub fn init_bank(
@@ -15,6 +17,10 @@ pub mod lending {
         liquidation_threshold: u64,
         max_ltv: u64,
     ) -> Result<()> {
-        instructions::process_init_bank(ctx, liquidation_threshold, max_ltv)
+        process_init_bank(ctx, liquidation_threshold, max_ltv)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        process_deposit(ctx, amount)
     }
 }
