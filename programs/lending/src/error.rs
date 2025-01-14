46d62d285cac078796e8dbec45076481dcda14f6
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2025-01-14 07:19:35 +0530

    OverRepay error added

--- programs/lending/src/error.rs
+++ programs/lending/src/error.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Insufficient funds to withdraw.")]
+    #[msg("Insufficient funds to withdraw")]
     InsufficientFunds,
+    #[msg("Over Repaying Borrowed amount")]
+    OverRepay,
 }
